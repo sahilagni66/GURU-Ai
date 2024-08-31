5cc2fdabaf29b20292670c84b98c1aa37bc5a405
sahilagni66 <145852964+sahilagni66@users.noreply.github.com>
2024-08-31 22:01:44 +0530

    Update gp-tagall.js

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -1,14 +1,8 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
-  let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-  m.reply(
-    `𓂃𓍴̷̸⃞🍉ᮬ᳝ Group : *${groupMetadata.subject}*\n  ׁ  ⃞🍂 Members : *${participants.length}*${text ? `\n  ׁ  ⃞🍂 Message : ${text}\n` : ''}\n┌───⊷ *MENTIONS*\n` +
-      users.map(v => ' ׄ ░⃟💽̸̲ @' + v.replace(/@.+/, '')).join`\n` +
-      '\n▬̸̷ᰱᰱ▬▬̥̥▬፟▭ ▬̸̷ᰱᰱ▬▬̥̥▬፟▭ ',
-    null,
-    {
-      mentions: users,
-    }
-  )
+    let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
+    m.reply(`🍀̵ׂׅ͜͡✨ 𝕲r᥆ᥙ⍴ : *${groupMetadata.subject}*\n\n🥬̸̶̊࣪͜⃝﹚ 𝕸ᥱmᑲᥱrs : *${participants.length}*${text ? `\n𒁷 𝕸ᥱssᥲgᥱ : ${text}\n` : ''}\n\n┌───⊷ 𝗠𝗘𝗡𝗧𝗜𝗢𝗡𝗦\n` + users.map(v => '𝆺𝅥𓌔⃝⃞̸̶🍊̸̶ @' + v.replace(/@.+/, '')).join`\n` + '\n𝕽ɑı𝘀ᥱׅ𐇵࣪𝖄𐐫υ𐔟࣪𐇵ׅＳρ𝗂𐔟𝗂𝗍', null, {
+        mentions: users
+    })
 }
 
 handler.help = ['tagall']
